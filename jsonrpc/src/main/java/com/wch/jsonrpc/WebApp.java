5b4621d57fadcf80fe44080782cca3175a461fa5
13295555958 <13295555958@qq.com>
2016-03-18 07:28:08 +0800

    'gradle'

---
+++ jsonrpc/src/main/java/com/wch/jsonrpc/WebApp.java
@@ -0,0 +1,14 @@
+package com.wch.jsonrpc;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by weichunhe on 2016/3/17.
+ */
+@SpringBootApplication
+public class WebApp {
+
+    public static void main(String[] args){
+
+    }
+}
