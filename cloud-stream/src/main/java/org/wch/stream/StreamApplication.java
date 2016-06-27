0498da87a8ec05e69799685d21f4889fc6845f1e
13295555958 <13295555958@qq.com>
2016-06-27 18:09:08 +0800

    sleuth-zipkin-stream

--- cloud-stream/src/main/java/org/wch/stream/StreamApplication.java
+++ cloud-stream/src/main/java/org/wch/stream/StreamApplication.java
@@ -2,10 +2,26 @@ package org.wch.stream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.sleuth.zipkin.ZipkinSpanReporter;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StreamApplication {
     public static void main(String[] args) {
         SpringApplication.run(StreamApplication.class, args);
+    }
+
+    // Use this for debugging (or if there is no Zipkin server running on port 9411)
+    @Bean
+    @ConditionalOnProperty(value = "sample.zipkin.enabled", havingValue = "false")
+    public ZipkinSpanReporter spanCollector() {
+        System.out.println("sample.zipkin.enabled");
+        return new ZipkinSpanReporter() {
+            @Override
+            public void report(zipkin.Span span) {
+                System.out.println((String.format("Reporting span [%s]", span)));
+            }
+        };
     }
 }
