9cfdd9998f11583491db79bc658d76209ee1571b
weichunhe <weichunhe@cmos.chinamobile.com>
2018-07-31 09:16:50 +0800

    proxy

--- hazelcast/src/main/java/org/wch/hazelcast/proxy/JDKTest.java
+++ hazelcast/src/main/java/org/wch/hazelcast/proxy/JDKTest.java
@@ -8,6 +8,7 @@ import java.lang.reflect.Proxy;
  * @author weichunhe
  *         Created on 2016/9/2.
  * @version 1.0
+ * 代理是针对类来说的，jdk 和cglib都可以对接口添加对应的实现，但是不属于代理，因为没有目标对象
  */
 public class JDKTest {
     public static void main(String[] args) {
