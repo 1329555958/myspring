8f2e8a912fe6d4916e80434c383f8ddf36e995e9
weichunhe <weichunhe@cmos.chinamobile.com>
2018-08-21 17:23:11 +0800

    fileupload

--- webtmpl/src/main/java/com/wch/controller/Demo.java
+++ webtmpl/src/main/java/com/wch/controller/Demo.java
@@ -1,18 +1,28 @@
 package com.wch.controller;
 
 import org.apache.commons.collections.map.HashedMap;
+import org.apache.commons.fileupload.disk.DiskFileItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.springframework.web.servlet.handler.AbstractHandlerMethodMapping;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 
-import java.lang.reflect.Method;
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 /**
@@ -43,6 +53,18 @@ public class Demo {
         Map<String, List<Map>> result = new LinkedHashMap<String, List<Map>>();
         extractMethodMappings(this.context, result);
         return result;
+    }
+    @RequestMapping("/upload")
+    public String upload(HttpServletRequest req) throws IOException {
+        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) req;
+        MultipartFile file = null;  // NOSONAR
+        file = multipartRequest.getFile("file");// 获取上传文件名
+
+        Path path = Paths.get("/tmp/" + file.getOriginalFilename());
+        path.toFile().createNewFile();
+       file.transferTo(path.toFile());
+
+        return "success";
     }
 
     protected void extractMethodMappings(ApplicationContext applicationContext,
